b844b5f4868f7aad8693af6756af4c44fc1eaf09
Hong Young Jun <hong5667@ajou.ac.kr>
2019-11-08 21:59:22 +0900

    홍영준 연동

---
+++ SampleHttp/app/src/main/java/org/techtown/http/MainActivity.java
@@ -0,0 +1,87 @@
+package org.techtown.http;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String urlStr = "http://192.168.0.22/test.php";
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request(urlStr);
+                    }
+                }).start();
+            }
+        });
+    }
+
+    public void request(String urlStr) {
+        StringBuilder output = new StringBuilder();
+        try {
+            URL url = new URL(urlStr);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            if (conn != null) {
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+
+                int resCode = conn.getResponseCode();//웹서버에 페이지 요청method
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line = null;
+                while (true) {
+                    line = reader.readLine();
+                    if (line == null) {
+                        break;
+                    }
+
+                    output.append(line + "\n");
+                }
+                reader.close();
+                conn.disconnect();
+            }
+        } catch (Exception ex) {
+            println("예외 발생함 : " + ex.toString());
+        }
+
+        println("응답 -> " + output.toString());
+    }
+
+    public void println(final String data) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView.append(data + "\n");
+            }
+        });
+
+    }
+
+}
