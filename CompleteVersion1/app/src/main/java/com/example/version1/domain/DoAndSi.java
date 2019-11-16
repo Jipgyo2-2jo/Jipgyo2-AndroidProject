6d537c43ddcf5c96659e525416a65a09004e900a
LeeHyunJu <dlguswn7175@ajou.ac.kr>
2019-11-16 17:28:01 +0900

    패키지 재구성, gps에 따른 미션활성화, 코스에 따른 polyline그리기 기능 완성

---
+++ CompleteVersion1/app/src/main/java/com/example/version1/domain/DoAndSi.java
@@ -0,0 +1,47 @@
+package com.example.version1.domain;
+
+public class DoAndSi {
+    String name;
+    double latitude;
+    double lonitude;
+    int zoomlevel;
+
+    public DoAndSi(String name, double latitude, double lonitude, int zoomlevel){
+        this.name = name;
+        this.latitude = latitude;
+        this.lonitude = lonitude;
+        this.zoomlevel = zoomlevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLonitude() {
+        return lonitude;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLonitude(double lonitude) {
+        this.lonitude = lonitude;
+    }
+
+    public int getZoomlevel() {
+        return zoomlevel;
+    }
+
+    public void setZoomlevel(int zoomlevel) {
+        this.zoomlevel = zoomlevel;
+    }
+}
---
+++ CompleteVersion1/app/src/main/java/com/example/version1/domain/Universities.java
@@ -0,0 +1,103 @@
+package com.example.version1.domain;
+
+public class Universities {
+    String id_num;
+    String 시도;
+    String 학교명;
+    String 학교명영문;
+    String 본분교;
+    String 설립;
+    String 주소;
+    String 전화번호;
+    String 홈페이지;
+    Double latitude;
+    Double lonitude;
+
+    public String getId_num() {
+        return id_num;
+    }
+
+    public void setId_num(String id_num) {
+        this.id_num = id_num;
+    }
+
+    public String get시도() {
+        return 시도;
+    }
+
+    public void set시도(String 시도) {
+        this.시도 = 시도;
+    }
+
+    public String get학교명() {
+        return 학교명;
+    }
+
+    public void set학교명(String 학교명) {
+        this.학교명 = 학교명;
+    }
+
+    public String get학교명영문() {
+        return 학교명영문;
+    }
+
+    public void set학교명영문(String 학교명영문) {
+        this.학교명영문 = 학교명영문;
+    }
+
+    public String get본분교() {
+        return 본분교;
+    }
+
+    public void set본분교(String 본분교) {
+        this.본분교 = 본분교;
+    }
+
+    public String get설립() {
+        return 설립;
+    }
+
+    public void set설립(String 설립) {
+        this.설립 = 설립;
+    }
+
+    public String get주소() {
+        return 주소;
+    }
+
+    public void set주소(String 주소) {
+        this.주소 = 주소;
+    }
+
+    public String get전화번호() {
+        return 전화번호;
+    }
+
+    public void set전화번호(String 전화번호) {
+        this.전화번호 = 전화번호;
+    }
+
+    public String get홈페이지() {
+        return 홈페이지;
+    }
+
+    public void set홈페이지(String 홈페이지) {
+        this.홈페이지 = 홈페이지;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLonitude() {
+        return lonitude;
+    }
+
+    public void setLonitude(Double lonitude) {
+        this.lonitude = lonitude;
+    }
+}
